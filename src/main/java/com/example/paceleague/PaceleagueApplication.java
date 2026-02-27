f96d7fd52d2eb84acb5a7b2fc965b9a378203efd
ataraxia-sko <39546834+ataraxia-sko@users.noreply.github.com>
2026-02-27 14:02:34 +0900

    init

---
+++ src/main/java/com/example/paceleague/PaceleagueApplication.java
@@ -0,0 +1,13 @@
+package com.example.paceleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaceleagueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaceleagueApplication.class, args);
+    }
+
+}
